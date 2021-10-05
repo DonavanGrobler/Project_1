24907e765dbd78ca10090a40c83ffb719e47b754
Donavan Grobler <donavangrobler2@gmail.com>
2021-10-05 20:35:55 +0200

    Session 2: AccountSystemWebSpringBoot - Error fixing

--- AccountSystemWebSpringBoot/src/main/java/za/ac/nwu/as/web/sb/controller/DemoController.java
+++ AccountSystemWebSpringBoot/src/main/java/za/ac/nwu/as/web/sb/controller/DemoController.java
@@ -1,6 +1,12 @@
 package za.ac.nwu.as.web.sb.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import za.ac.nwu.as.domain.service.GeneralResponse;
 
+@RestController
 
 public class DemoController {
+    @GetMapping("/ping")
+    public GeneralResponse<String> getAll(){return new GeneralResponse<String>(true, "No types found"); }
 }

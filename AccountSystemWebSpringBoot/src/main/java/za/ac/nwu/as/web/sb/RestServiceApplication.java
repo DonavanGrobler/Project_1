475d9d4562e869f8929a6de8b75fd33630f029b2
Donavan Grobler <donavangrobler2@gmail.com>
2021-10-05 21:32:37 +0200

    Session 2: AccountSystemWebSpringBoot - NO MORE ERRORS :)

--- AccountSystemWebSpringBoot/src/main/java/za/ac/nwu/as/web/sb/RestServiceApplication.java
+++ AccountSystemWebSpringBoot/src/main/java/za/ac/nwu/as/web/sb/RestServiceApplication.java
@@ -2,7 +2,10 @@ package za.ac.nwu.as.web.sb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-@SpringBootApplication
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+//@SpringBootApplication
 public class RestServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(RestServiceApplication.class, args);
